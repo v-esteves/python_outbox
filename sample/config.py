c12e7f466499fb132019eed636ff26eba4c8ca33
Vitor <vestevesaws@gmail.com>
2021-10-16 12:01:10 +0100

    Initial commit. WIP. Refactor registration.py to be done next.

---
+++ sample/config.py
@@ -0,0 +1,4 @@
+"""Flask app configurations"""
+
+REDIS_PORT = 6379
+REDIS_ENDPOINT = 'localhost'
--- sample/registration.py
+++ sample/registration.py
@@ -2,21 +2,27 @@ import redis
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
 
 @app.route('/api/v1/user', methods=['POST'])
 def registration():
     json = request.get_json()
-    
-    name = json["name"]
-    email = json["email"]
 
-    db = redis.Redis(host='localhost',port=6379, charset="utf-8", decode_responses=True)
+    # Connect to Redis 
+    db = redis.Redis(host=app.config['REDIS_ENDPOINT'],
+                        port=app.config['REDIS_PORT'], 
+                        charset="utf-8", 
+                        decode_responses=True)
 
+    # Create a "transaction" to execute both commands or don't execute
+    # We need to have total assurance that both data is stored
     transaction = db.pipeline()
     transaction.set(json["email"],json["name"])
+    # Redis Lists are used to create a queue for the dispatcher work
     transaction.rpush('emails',json["email"])
     transaction.execute()
 
-    nameFromRedis = db.get(json["name"])
+    # Fetch name to return
+    nameFromRedis = db.get(json["email"])
     
-    return "JSON name sent: "+str(nameFromRedis)
+    return "Name sent: "+str(nameFromRedis)
